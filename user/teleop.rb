b0989942d49fc93e543db68d5c6d00911f997bba
Cyberarm <matthewlikesrobots@gmail.com>
2018-02-23 11:49:35 -0600

    Added support for another controler

--- user/teleop.rb
+++ user/teleop.rb
@@ -7,54 +7,12 @@ class TeleOp < RoverProject::Program
   end
 
   def loop
-    if input(:gamepad)
-      motor(:left_drive).set_power(input(:gamepad).left_stick_y)
-      motor(:right_drive).set_power(input(:gamepad).right_stick_y)
-      # log("GAMEPAD", "left Y: #{input(:gamepad).left_stick_y}, right Y: #{input(:gamepad).right_stick_y}")
+    if input(:gamepad) && input(:gamepad).ready
+      motor(:left_rear_drive).set_power(input(:gamepad).left_stick_y)
+      motor(:right_rear_drive).set_power(input(:gamepad).left_stick_y)
 
-      # puts "Axis leftX: #{input(:gamepad).left_stick_x}"
-      # puts "Axis leftY: #{input(:gamepad).left_stick_y}"
-      # puts "Axis rightX: #{input(:gamepad).right_stick_x}"
-      # puts "Axis rightY: #{input(:gamepad).right_stick_y}"
-      # puts "Axis leftTrigger: #{input(:gamepad).left_trigger}"
-      # puts "Axis rightTrigger: #{input(:gamepad).right_trigger}"
-      # puts "Last loop time: #{last_loop_time}s"
-    end
-
-    if input(:keyboard) && (!input(:gamepad).ready)
-      if input(:keyboard).holding?("W")
-        motor(:left_drive).set_power(200)
-        motor(:right_drive).set_power(200)
-      elsif input(:keyboard).holding?("S")
-        motor(:left_drive).set_power(-200)
-        motor(:right_drive).set_power(-200)
-      elsif input(:keyboard).holding?("A")
-        motor(:left_drive).set_power(-200)
-        motor(:right_drive).set_power(200)
-      elsif input(:keyboard).holding?("D")
-        motor(:left_drive).set_power(200)
-        motor(:right_drive).set_power(-200)
-      else
-        motor(:left_drive).set_power(0)
-        motor(:right_drive).set_power(0)
-      end
-    end
-
-    if input(:keyboard) && input(:keyboard).holding?("W")
-      # puts "W is pressed."
-    end
-    if input(:keyboard) && input(:keyboard).released?("Tab")
-      puts "Tab is released."
-    end
-    if input(:keyboard) && input(:keyboard).released?("W")
-      puts "W is released."
-    end
-
-    if input(:mouse)
-      if input(:mouse).released?(RoverProject::Input::Mouse::LEFT_BUTTON)
-        puts "Left Mouse is released."
-      end
-      # puts "Mouse X: #{input(:mouse).x}, Y: #{input(:mouse).y}"
+      motor(:left_front_drive).set_power(input(:gamepad).right_stick_y)
+      motor(:right_front_drive).set_power(input(:gamepad).right_stick_y)
     end
   end
 
